f87c778f375245e61a64324e61a01b4c88738998
Ross Light <light@yourbase.io>
2020-09-04 12:10:17 -0700

    http/headers: new package (#5)
    Moved HTTP-related packages under http/ directory. Kept forwarding packages
    so callers can migrate gradually.

---
+++ http/headers/headers_test.go
@@ -0,0 +1,50 @@
+// Copyright 2020 YourBase Inc.
+// SPDX-License-Identifier: BSD-3-Clause
+
+package headers
+
+import (
+	"net/http"
+	"testing"
+)
+
+func TestCanonical(t *testing.T) {
+	constants := []string{
+		Accept,
+		AcceptCharset,
+		AcceptEncoding,
+		AcceptLanguage,
+		AcceptRanges,
+		Age,
+		Allow,
+		Authorization,
+		CacheControl,
+		ContentDisposition,
+		ContentEncoding,
+		ContentLength,
+		ContentRange,
+		ContentType,
+		ETag,
+		Expires,
+		IfMatch,
+		IfModifiedSince,
+		IfNoneMatch,
+		IfRange,
+		IfUnmodifiedSince,
+		LastModified,
+		Location,
+		Range,
+		RetryAfter,
+		TransferEncoding,
+		Vary,
+		WWWAuthenticate,
+		XForwardedFor,
+		XForwardedHost,
+		XForwardedProto,
+	}
+	for _, c := range constants {
+		if want := http.CanonicalHeaderKey(c); want != c {
+			t.Errorf("Canonical form of %q is %q", c, want)
+		}
+	}
+}
